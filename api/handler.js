933766a281a92da24fe872101f4ae8100dc7bb26
Milan Frolov <milan.frolov@gmail.com>
2024-10-30 00:48:03 +0300

    first commit

---
+++ api/handler.js
@@ -0,0 +1,7 @@
+// api/handler.js
+export default function handler(req, res) {
+    const currentUrl = req.query.url; // Assuming you send the URL as a query parameter
+    // Process the URL or perform your logic here
+    res.status(200).json({ message: 'URL received', url: currentUrl });
+  }
+  
