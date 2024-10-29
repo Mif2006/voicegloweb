71c694741fcd1b919465baddd44b0eaa5db3e5c7
Mif2006 <114983743+Mif2006@users.noreply.github.com>
2024-10-30 01:28:07 +0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -2,6 +2,8 @@
 export default function handler(req, res) {
     const currentUrl = req.query.url; // Assuming you send the URL as a query parameter
     // Process the URL or perform your logic here
+    console.log(currentUrl)
     res.status(200).json({ message: 'URL received', url: currentUrl });
+
   }
   
